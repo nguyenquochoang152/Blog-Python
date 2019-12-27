292ec0628262c317d03537bfc0a3fe406e64bd55
nguyenquochoang152 <quochoang1521998@gmail.com>
2019-12-28 03:17:36 +0700

    comment

--- Django_Blog/blog/urls.py
+++ Django_Blog/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name= "blog"),
-    path('<int:id>/', views.post, name= "post"),
+    path('<int:pk>/', views.post, name= "post"),
 ]   
--- Django_Blog/blog/views.py
+++ Django_Blog/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .models import Post, Comment
+from blog.forms import CommentForm
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 
 # Create your views here.
@@ -9,6 +11,14 @@ class PostListView(ListView):
     template_name = 'blog/blog.html'
     context_object_name = 'Posts'
     paginate_by = 3 
-def post(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, 'blog/post.html', {'post':post})
+    
+def post(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST,author=request.user,post=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    return render(request, "blog/post.html", {"post":post, "form":form})
+
